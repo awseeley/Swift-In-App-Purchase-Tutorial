853e3cbaed2548011e89893489741bd5562d0be4
Andrew Seeley <awseeley@gmail.com>
2014-12-08 19:22:10 +1100

    Initial Commit

---
+++ SeemuIAPTute/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SeemuIAPTute
+//
+//  Created by Andrew Seeley on 8/12/2014.
+//  Copyright (c) 2014 Seemu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
